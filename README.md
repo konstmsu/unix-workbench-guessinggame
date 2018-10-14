43ad2a0593b88507740ff18102ad3e16893e0675
Konstantin Spirin <konstmsu@gmail.com>
2018-10-14 22:00:39 +0800

    Implemented guessing game and makefile

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 h1. Guessing Game
-*Created at Sun Oct 14 03:39:20 +08 2018*
-There are 7 lines in guessinggame.sh
+*Created at Sun Oct 14 21:59:50 +08 2018*
+There are 21 lines in guessinggame.sh
--- guessinggame.sh
+++ guessinggame.sh
@@ -2,6 +2,20 @@
 
 while :
 do
-	read -p "Guess how many files are in $PWD:" guess
-	echo $guess
+	read -p "Guess the number of files in $PWD: " guess
+
+	actual=$(ls | wc -l)
+
+	if [ $actual -eq $guess ]
+	then
+		echo "Congratulations, your guess is correct!"
+		break
+	elif [ $actual -lt $guess ]
+	then
+		hint="lower"
+	else
+		hint="higher"
+	fi
+
+	echo "The actual number is $hint than $guess, please try again"
 done
